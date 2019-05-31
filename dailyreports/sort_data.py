60ef64de54d4d8d4bf942fb6772aaa0f98bc37fc
ShravanNaidu <naidushravan@johndeere.com>
2019-05-31 15:36:27 -0500

    Changing csv file and index

--- dailyreports/sort_data.py
+++ dailyreports/sort_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-data = pd.read_csv("SalesJan2009.csv")
-data.drop_duplicates(subset="Product", keep='first', inplace=True)
-data.to_csv("SalesSorted.csv")
+data = pd.read_csv("screener_data.csv")
+data.drop_duplicates(subset="Ticker", keep='first', inplace=True)
+data.to_csv("screener_data.csv")
