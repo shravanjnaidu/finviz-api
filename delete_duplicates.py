8b3d503d0ae53cbeaf42083c3efbdb98b8ae547d
Shravan Naidu <shravannaidu@Shravans-Air.domain>
2019-06-05 21:21:47 -0500

    Combining formatiing of columns and deleting duplicates

--- delete_duplicates.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-path = 'dailyreports/screener_data.csv'
-with open(path) as f:
-  data = list(csv.reader(f))
-  new_data = [a for i, a in enumerate(data) if a not in data[:i]]
-  with open(path, 'w') as t:
-     write = csv.writer(t)
-     write.writerows(new_data)
