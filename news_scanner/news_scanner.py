ba9800e34c89cd8a001f1c9fd91fb22d72d5c1ff
Shravan Naidu <shravannaidu@Shravans-Air.domain>
2019-06-15 13:32:35 -0500

    Adding news download and delete feature

--- news_scanner/news_scanner.py
+++ news_scanner/news_scanner.py
@@ -3,8 +3,11 @@ import os
 import requests
 import urllib.request
 
+def download_news():
+        os.system("curl https://finviz.com/news.ashx -o news.html")
+
 def news_scanner():
-    stringToMatch = ['Uber', 'Shravan']
+    stringToMatch = ['Uber', 'Fed']
     # news_line = ''
     for keyword in stringToMatch:
         with open('news.html', 'r') as file:
@@ -15,4 +18,9 @@ def news_scanner():
                     end = '" target'
                     print(keyword + " : " + news_line[news_line.find(start)+len(start):news_line.rfind(end)])
 
+def delete_news():
+        os.system("rm -rf news.html")
+
+download_news()
 news_scanner()
+delete_news()
